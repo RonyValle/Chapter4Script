434e60c2a1478849d57175be627d5076d0c99f8b
RonyValle <rony28v@icloud.com>
2018-02-06 17:08:33 -0600

    updated ch4 with comments

--- ch4.sh
+++ ch4.sh
@@ -2,7 +2,8 @@
 #Rony Valle Lopez
 #Chapter4 Script
 
-#the following script allows you to see your Devices that can be formatted and it also starts fdisk so you can format your device.
+#the following script allows you to see your Devices that can be formatted 
+and it also starts fdisk so you can format your device.
 
 #the amount of space available in your devices
 
@@ -10,14 +11,17 @@
 echo
 echo available disk space in your devices:
 echo
-
+#displays available disk space
 df
 
 echo
 echo choose the device you want to format
 echo
+#command that shows you attached storage devices
 lsblk 
 echo
 echo create you partition using fdisk
 echo
+#this command starts the program fdisk which allows you to format a storage device
+and allows the creation of partition tables. 
 fdisk
